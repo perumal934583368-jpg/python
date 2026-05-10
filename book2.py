b861da781cc3c1d5f19e0af8e84c01f87f62b55f
perumal934583368-jpg <perumal934583368@gmail.com>
2026-05-10 18:03:01 +0530

    Add files via upload

---
+++ book2.py
@@ -0,0 +1,41 @@
+"""
+a=4
+b=5
+print(a>=b)"""
+"""
+a=10
+if(a>11):
+    print("anu")
+else:
+    print("anbu")"""
+"""
+a=10
+b=20
+if(a>b):
+    print("anu")
+else:
+    print("ammu")"""
+
+#logical operator
+"""
+a=10
+if(a<20 and a>20 and a<4):
+    print("apple")
+else:
+    print("ammu")"""
+# or gate
+"""
+a=2
+b=3
+if(a>b or a>b):
+    print("honey")
+else:
+    print("puppy")"""
+#
+"""
+a=5
+b=3
+if(not(a<2 and a>6 and a<4)):
+    print("anna")
+else:
+    print("sistr")"""
