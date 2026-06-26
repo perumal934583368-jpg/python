2f2c6a2b35c9614ff50424eae777b077fa82f399
perumal934583368-jpg <perumal934583368@gmail.com>
2026-06-26 02:24:13 +0100

    Add files via upload

---
+++ lab 4.py
@@ -0,0 +1,71 @@
+"""name="Anusha"
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.swapcase())"""
+"""print(1.question)
+string=("anusha:")
+print(string[::-1])"""
+"""(2.question)
+word=input("enter word:")
+reverse=word[::-1]
+if reverse==word:
+    print(word,"palindrom")
+else:
+    print(word,"not palindrom")"""
+
+"""#cout vowels and consoants(3)
+word=input("enter word or letter:")
+vowels=0
+consonants=0
+for letter in word.lower():
+    if letter in "aeiou":
+        vowels = vowels + 1
+        print("vowels=", vowels)
+    elif letter.isalpha():
+        consoants = consonants + 1
+        print("consoants =", consoants)"""
+"""print (5.question)
+word =input("enter a word:")
+for character in word:
+    if word.count(charactr) ==1:
+        print(character)
+        break"""
+"""#print(4 question)
+word=input("enter a word:")
+new_word =""
+for letter in word:
+    if letter not in new_word:
+        new_word =new_word + letter
+        print(new_word)"""
+"""(question 6)
+word =input("enter a word:")
+for character in set(word):
+    print(character,"=",word.count(character))"""
+"""(question 7)
+word1=input("enter first word:")
+word2=input("enter a second word:")
+if sorted(word1)==sorted(word2):
+    print("anagram")
+else:
+ print("not an anagram")"""
+"""(question 8)
+anu = input("enter a word:")
+print(anu.replace("","-"))"""
+
+
+
+
+
+
+
+
+
+
+    
+ 
+
+        
+  
